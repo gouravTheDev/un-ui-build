8b69a260d024d39c11aa03bc3e5ea720c2e041cb
gourav <chatterjeegouravking@gmail.com>
2021-05-06 23:38:14 +0530

    FIrst

---
+++ precache-manifest.4ac249999aba412f8d4427c0f20248ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc7c6ff8026909612db904d2f371ee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257c550af6b878c10430",
+    "url": "/static/css/main.b736f87c.chunk.css"
+  },
+  {
+    "revision": "75be429ecf089bc3d3e6",
+    "url": "/static/js/2.7698dd79.chunk.js"
+  },
+  {
+    "revision": "3f4adf4004dcae015feb384f6b36261b",
+    "url": "/static/js/2.7698dd79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257c550af6b878c10430",
+    "url": "/static/js/main.b70801ca.chunk.js"
+  },
+  {
+    "revision": "ff284cfb6fd7eb40bcc1",
+    "url": "/static/js/runtime-main.e2142da2.js"
+  }
+]);
